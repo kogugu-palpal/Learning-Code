8980c13f7085e51467d7350b1d06bfa5a678dd86
GuGu_OX <kogyipalpal@gmail.com>
2025-08-06 12:27:36 +0700

    edit extension and learn new

---
+++ python/variable.py
@@ -0,0 +1,9 @@
+color = "Blue"
+# print ("Your favorite color is", + color)
+# This code prints the user's favorite color but has a syntax error.
+# The '+' operator is incorrectly used here.
+# The correct way to concatenate strings in Python is to use a comma or the '+' operator without the '+' before the variable.
+# Corrected code:
+print("Your favorite color is " + color)
+# or
+print("Your favorite color is", color)
