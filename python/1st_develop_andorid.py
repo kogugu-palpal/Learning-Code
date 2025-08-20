34e055c16c20f07f8e64860a37b5107db76f75db
GuGu_OX <kogyipalpal@gmail.com>
2025-08-20 18:31:39 +0700

    not finish app

--- python/1st_develop_andorid.py
+++ python/1st_develop_andorid.py
@@ -2,7 +2,14 @@ from kivy.app import App
 
 class GreetingApp(App):
 
-    pass
+    def clear(self, widget_to_clear):
+        widget_to_clear.text =''
+
+    def hello(self, widget_to_greet):
+        widget_to_greet.text = "Hello, How are you"
+
+        
+    # pass
 
 if __name__ == '__main__':
     GreetingApp().run()
