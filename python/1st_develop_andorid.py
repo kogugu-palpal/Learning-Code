6922532d5dd47aba293689be05a40acffe10c615
GuGu_OX <kogyipalpal@gmail.com>
2025-08-22 13:20:07 +0700

    newkv file add

--- python/1st_develop_andorid.py
+++ python/1st_develop_andorid.py
@@ -5,8 +5,9 @@ class GreetingApp(App):
     def clear(self, widget_to_clear):
         widget_to_clear.text =''
 
-    def hello(self, widget_to_greet):
-        widget_to_greet.text = "Hello, How are you"
+    def hello(self, name_input_widget, hello_label_widget):
+        name_to_greet = name_input_widget.text
+        hello_label_widget.text = "Hello, How are you, "+ name_to_greet + "!"
 
         
     # pass
